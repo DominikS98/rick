0eb0c0cf5fa492d4b660bc880ad93f336d0593a0
Dominik Szojda <szojda.98@gmail.com>
2022-03-15 14:11:16 +0100

    mobile styles and faind by name

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-const characters = document.getElementById('characters');
-const locations = document.getElementById('locations');
-const episodes = document.getElementById('episodes');
-const contnentBox = document.getElementById('contnent');
-const next = document.getElementById('next');
-const previous = document.getElementById('previous');
+const characters = document.querySelector('#characters');
+const locations = document.querySelector('#locations');
+const episodes = document.querySelector('#episodes');
+const contnentBox = document.querySelector('#contnent');
+const next = document.querySelector('#next');
+const previous = document.querySelector('#previous');
+const input = document.querySelector('#find-data');
+const burger = document.querySelector('.burger');
+
+
+burger.addEventListener('click', () => {
+    const nav = document.querySelector('header nav');
+    nav.classList.toggle("open");
+})
 
 let page = 1;
 let state = '';
 
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 function find(url) {
 
@@ -28,37 +39,38 @@ function addToDomCH(data) {
     const box = document.getElementById('temp-box');
     const card = document.importNode(box.content, true);
     card.getElementById('img').src = data.image;
-    card.getElementById('name').innerHTML = 'name:' + data.name;
-    card.getElementById('gender').innerHTML = 'gender:' + data.gender;
-    card.getElementById('species').innerHTML = 'species:' + data.species;
-    card.getElementById('status').innerHTML = 'status:' + data.status;
+    card.getElementById('name').innerHTML = data.name;
+    card.getElementById('gender').innerHTML = 'gender: ' + data.gender;
+    card.getElementById('species').innerHTML = 'species: ' + data.species;
+    card.getElementById('status').innerHTML = 'status: ' + data.status;
     contnentBox.append(card);
 }
 
 function addToDomLO(data) {
     const box = document.getElementById('temp-box');
     const card = document.importNode(box.content, true);
-    card.getElementById('name').innerHTML = data.name;
-    card.getElementById('gender').innerHTML = data.type;
-    card.getElementById('species').innerHTML = data.dimension;
-    card.getElementById('status').innerHTML = data.created;
+    card.getElementById('name').innerHTML = 'name: ' + data.name;
+    card.getElementById('gender').innerHTML = 'type: ' + data.type;
+    card.getElementById('species').innerHTML = 'dimension: ' + data.dimension;
+    card.getElementById('status').innerHTML = 'created: ' + data.created;
     contnentBox.append(card);
 }
 
 function addToDomEP(data) {
     const box = document.getElementById('temp-box');
     const card = document.importNode(box.content, true);
-    card.getElementById('name').innerHTML = data.name;
-    card.getElementById('gender').innerHTML = data.air_date;
-    card.getElementById('species').innerHTML = data.episode;
-    card.getElementById('status').innerHTML = data.characters;
+    card.getElementById('name').innerHTML = 'name: ' + data.name;
+    card.getElementById('gender').innerHTML = 'air_date: ' + data.air_date;
+    card.getElementById('species').innerHTML = 'episode: ' + data.episode;
+    card.getElementById('status').innerHTML = 'characters: ' + data.characters;
     contnentBox.append(card);
 }
 
 async function charactersData() {
-    state = 'charactersData';
+    state = 'character';
     delteNode();
     const charactersTable = await find('https://rickandmortyapi.com/api/character/?page=' + page);
+    //const charactersTable = await find('https://rickandmortyapi.com/api/character/?name=rick');
     charactersTable.results.forEach(element => {
         const data = {
             name: element.name,
@@ -71,7 +83,7 @@ async function charactersData() {
     });
 }
 async function locationsData() {
-    state = 'locationsData';
+    state = 'location';
     delteNode();
     const locationsTable = await find('https://rickandmortyapi.com/api/location/?page=' + page);
     locationsTable.results.forEach(element => {
@@ -86,7 +98,7 @@ async function locationsData() {
     });
 }
 async function episodesData() {
-    state = 'episodesData';
+    state = 'episode';
     delteNode();
     const episodesTable = await find('https://rickandmortyapi.com/api/episode/?page=' + page);
     episodesTable.results.forEach(element => {
@@ -99,12 +111,49 @@ async function episodesData() {
         addToDomEP(data);
     });
 }
+async function FaindChara(url) {
+    delteNode();
+    const charactersTable = await find(`https://rickandmortyapi.com/api/${state}/?name=` + url);
+    charactersTable.results.forEach(element => {
+        if (state === 'episode') {
+            const data = {
+                name: element.name,
+                air_date: element.air_date,
+                episode: element.episode,
+                characters: element.characters,
+            };
+            addToDomEP(data);
+        }
+        else if (state === 'location') {
+            const data = {
+                name: element.name,
+                type: element.type,
+                dimension: element.dimension,
+                residents: element.residents,
+                created: element.created,
+            };
+            addToDomLO(data);
+        }
+        else {
+            const data = {
+                name: element.name,
+                gender: element.gender,
+                species: element.species,
+                status: element.status,
+                image: element.image,
+            };
+            addToDomCH(data);
+        }
+
+    });
+}
+
 function NextPageCh() {
 
     page++;
-    if (state === 'episodesData')
+    if (state === 'episode')
         episodesData();
-    else if (state === 'locationsData')
+    else if (state === 'location')
         locationsData();
     else
         charactersData();
@@ -112,16 +161,22 @@ function NextPageCh() {
 function previousPageCh() {
 
     page--;
-    if (state === 'episodesData')
+    if (state === 'episode')
         episodesData();
-    else if (state === 'locationsData')
+    else if (state === 'location')
         locationsData();
     else
         charactersData();
 }
+
 
 characters.addEventListener('click', charactersData);
 locations.addEventListener('click', locationsData);
 episodes.addEventListener('click', episodesData);
 next.addEventListener('click', NextPageCh);
 previous.addEventListener('click', previousPageCh);
+input.addEventListener('keyup', (event) => {
+    if (event.keyCode === 13) {
+        FaindChara(input.value);
+    }
+});
